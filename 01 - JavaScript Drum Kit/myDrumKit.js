566a35b479e1ace219c64dbfc2ec2940e86e6055
cdsalty <soltiscd@gmail.com>
2019-08-24 00:47:05 -0400

    working to removeTransition

--- 01 - JavaScript Drum Kit/myDrumKit.js
+++ 01 - JavaScript Drum Kit/myDrumKit.js
@@ -4,13 +4,23 @@
 window.addEventListener('keydown', ((e) => {
   //   console.log(e);
   //   console.log(e.keyCode);  // this leads us and should lead our thinking towards...
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // allow us to get the event's keycode
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // gets the "audio" keycode
     // console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if(!audio) return;  // if there is no pre-defined audio for that keyCode, return and do nothing
+  audio.currentTime = 0; // rewind to the start of the audio anytime pressed
   // To play the audio element selected, simply play()
-  audio.play();
+  audio.play(); // if you play an audio element while it's playing, it won't play. we use 'curent time'
+  // console.log(key);
+  key.classList.add('playing');
 }));
 
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+}
 
-
-
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); // next, create what remove Transistion needs to do... 
+  // transitionend, which deals with the css transition taking place, when transitionend is happing, then removeTransition function is called
